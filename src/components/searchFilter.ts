3fe0ab889b1612d951b1c4c23fb4d40bf22aacbb
Pablo J Torrubiano <me@pablotorrubiano.xyz>
2022-05-01 22:29:42 +0200

    Improved search filters

--- src/components/searchFilter.ts
+++ src/components/searchFilter.ts
@@ -4,12 +4,16 @@ const {
   MAX_PRICE: maxPrice,
   FROM: from,
   TO: to,
+  BLACKLISTED_IDS: blacklistedIds,
 } = process.env;
+
+const blacklistedIdsArray = blacklistedIds && JSON.parse(blacklistedIds);
 
 const matchesFilters = (apartment: Apartment) => {
   if (maxPrice && apartment.price > parseInt(maxPrice, 10)) return false;
   if (from && apartment.dates.from > new Date(from)) return false;
   if (to && apartment.dates.to && apartment.dates.to < new Date(to)) return false;
+  if (blacklistedIdsArray && blacklistedIdsArray.includes(apartment.postedBy)) return false;
   return true;
 };
 
--- src/components/telegram.ts
+++ src/components/telegram.ts
@@ -5,13 +5,14 @@ const bot = new TeleBot(process.env.TELEGRAM_TOKEN || '');
 
 export const sendApartmentAlert = (apartment: Apartment) => bot.sendMessage(
   process.env.TELEGRAM_CHAT_ID || '',
-  `New apartment found:\n
-  ${apartment.title}\n
-  ${apartment.subtitle}\n
-  Price: ${apartment.price} eur\n
-  From: ${new Date(apartment.dates.from).toDateString()}\n
-  To: ${apartment.dates.to ? new Date(apartment.dates.to).toDateString() : ''}\n
-  \n
+  `New apartment found:
+  ${apartment.title}
+  ${apartment.subtitle}
+  Price: ${apartment.price} eur
+  Size: ${apartment.size} m2
+  From: ${new Date(apartment.dates.from).toDateString()}
+  To: ${apartment.dates.to ? new Date(apartment.dates.to).toDateString() : '--'}
+  Posted by: ${apartment.postedBy}\n
   ${apartment.url}`,
 );
 
